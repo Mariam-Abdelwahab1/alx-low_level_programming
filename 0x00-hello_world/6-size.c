4cf900a4ee9ec774b316fed55908b87b08c73d21
mariam <mariamabdelwahab30@gmail.com>
2026-09-25 10:43:31 +0300

    My 3rd C programming file.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
- /**
-  * main - Prints size of various types
-  *
-  * Return: Always 0 (Success)
-  */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
